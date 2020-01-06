b97d490cbae7b601b382eaaaa78bb5319311b485
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2020-01-06 00:02:39 -0500

    init commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Esmaeil MIRZAEE on 05/01/2020.
+//  Copyright © 2020 Esmaeil MIRZAEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      Text("Welcome to my first app.")
+        .fontWeight(.heavy)
+        .foregroundColor(Color.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
